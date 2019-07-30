d11ada25f3b21e94eeab5e4626c2586df765104a
Sean McCann <sean89@gmail.com>
2019-07-30 10:25:07 -0700

    Use panic

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 extern crate chrono;
 extern crate time;
 
-use chrono::{Utc};
+use chrono::Utc;
 use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
     let mut n = 0;
 
     if args.len() > 1 {
         n = args[1].parse::<i64>().unwrap();
     };
 
-    if n > 0 {
-        let d = Utc::now() + time::Duration::days(n);
-        println!("{}", d.format("%a %b %e, %Y"));
-    } else {
-        println!("Can only look forward to the future.")
-    }
+    if n < 0 {
+        panic!("Can only look forward to the future.")
+    };
+
+    let d = Utc::now() + time::Duration::days(n);
+    println!("{}", d.format("%a %b %e, %Y"));
 }
